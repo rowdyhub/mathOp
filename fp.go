0e7b5d6af41a6af2a365ff2ceea65b6261261bd6
SergeyN <serega-nasibulin@yandex.ru>
2020-09-08 22:55:39 +0400

    Change programm loop, update code

--- fp.go
+++ fp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // Возведение числа "a" в степень "b".
@@ -15,65 +16,57 @@ func exponentiation(a int, b int) int {
 	}
 	return c
 }
+// Квадратный корень из числа "a"
+//func sqr(a int) int {
+//	
+//}
 // Вычисление факториала числа "a"
 func factorial(a int) int {
-	var i int = 1
-	var b int = 1
-	for i <= a {
-		b = b * i
-		i = i + 1
+	if a > 20 {
+		var err string
+		err = "Предел вычисляемого значения: 20"
+		return err
+	} else {
+		var i int = 1
+		var b int = 1
+		for i <= a {
+			b = b * i
+			i = i + 1
+		}	
+		return b
 	}
-	return b
-}
-func restart(){
-	var comand string
-	fmt.Print("\n---------------------------------------------------\n")
-	fmt.Print("Введите команду: \n")
-	fmt.Fscan(os.Stdin, &comand)
-
-	switch comand{
-		case "help":
-			fmt.Print("---------------------------------------------------\n")
-			fmt.Print("Список доступных команд:\n")
-			fmt.Print("exit - выход;\n")
-			fmt.Print("help - список команд;\n")
-			fmt.Print("f - вычесление факториала;\n")
-			fmt.Print("e - возведение в степень;\n")
-			restart()
-		case "f":
-			var atr int
-			var c int
-			fmt.Print("---------------------------------------------------\n")
-			fmt.Print("Вывести факториал числа: ")
-			fmt.Fscan(os.Stdin, &atr)
-			c = factorial(atr)
-			fmt.Print("Ответ: ")
-			fmt.Print(c)
-
-			restart()
-		case "e":
-			var atr1 int
-			var atr2 int
-			var c int
-			fmt.Print("---------------------------------------------------\n")
-			fmt.Print("Возвести число: ")
-			fmt.Fscan(os.Stdin, &atr1)
-			fmt.Print("В степень: ")
-			fmt.Fscan(os.Stdin, &atr2)
-			c = exponentiation(atr1, atr2)
-			fmt.Print("Ответ: ")
-			fmt.Print(c)
-
-			restart()			
-		case "exit":
-			os.Exit(0)
-		default:
-			fmt.Print("---------------------------------------------------\n")
-			fmt.Print("Команда не найдена! Введите help для вывода списка команд\n")
-			restart()
-}
 }
 
 func main() {
-	restart()
+	for true {
+		var comand string
+		fmt.Print("\n---------------------------------------------------\nВведите команду: \n")
+		fmt.Fscan(os.Stdin, &comand)
+
+		switch comand{
+			case "help":
+				fmt.Print("---------------------------------------------------\nСписок доступных команд:\nexit - выход;\nhelp - список команд;\nf - вычесление факториала;\ne - возведение в степень;\n")
+			case "f":
+				var atr int
+				var c int
+				fmt.Print("---------------------------------------------------\nВывести факториал числа: ")
+				fmt.Fscan(os.Stdin, &atr)
+				c = factorial(atr)
+				fmt.Print("Ответ: " + strconv.Itoa(c))
+			case "e":
+				var atr1 int
+				var atr2 int
+				var c int
+				fmt.Print("---------------------------------------------------\nВозвести число: ")
+				fmt.Fscan(os.Stdin, &atr1)
+				fmt.Print("В степень: ")
+				fmt.Fscan(os.Stdin, &atr2)
+				c = exponentiation(atr1, atr2)
+				fmt.Print("Ответ: " + strconv.Itoa(c))
+			case "exit":
+				break
+			default:
+				fmt.Print("---------------------------------------------------\nКоманда не найдена! Введите help для вывода списка команд\n")
+		}
+	}
 }
